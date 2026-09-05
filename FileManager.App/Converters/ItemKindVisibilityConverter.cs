599fe4a70c59681068d50e304bbb132b89b9b72d
Vuk Drazic <vukdrazic77@gmail.com>
2026-09-05 14:06:30 +0200

    feat(app): implement ItemKindVisibilityConverter for different Detail panel UI depending on File or Folder kind

---
+++ FileManager.App/Converters/ItemKindVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Data.Converters;
+using FileManager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FileManager.App.Converters
+{
+    public class ItemKindVisibilityConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => value is StorageItem item && item.Kind.ToString() == parameter as string;
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
